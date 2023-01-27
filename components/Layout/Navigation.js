32aea0a6a081fbaa703da017529a4de901bdc24f
n-els <nels@tuta.io>
2023-01-27 09:19:03 +0100

    add header menu for wider screens

--- components/Layout/Navigation.js
+++ components/Layout/Navigation.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import MobileMenu from './MobileMenu';
+import WideMenu from './WideMenu';
+
+const links = [
+  {
+    text: 'Start',
+    href: '/',
+  },
+  {
+    text: 'Projekte',
+    href: '/',
+  },
+  {
+    text: 'Fotos',
+    href: '/',
+  },
+  {
+    text: 'Kontakt',
+    href: '/',
+  },
+];
 
 const Navigation = () => {
-  return <MobileMenu />;
+  return (
+    <>
+      <div className="md:hidden">
+        <MobileMenu />
+      </div>
+      <div className="hidden md:block">
+        <WideMenu links={links} />
+      </div>
+    </>
+  );
 };
 
 export default Navigation;
--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="px-6 text-center tracking-wide">
-        <div className="mt-4 sm:mt-12 mb-6 flex flex-col items-center sm:flex-row sm:justify-center w-full mx-auto sm:gap-10">
+      <section className=" tracking-wide">
+        <div className="mt-4 sm:mt-12 mb-6 flex flex-col-reverse items-center md:flex-row md:justify-between w-full mx-auto">
+          <div>
+            <h2 className=" text-4xl mt-4 tracking-wide">
+              Ich bin <span className="font-bold">Nader el Sayed</span>
+            </h2>
+
+            <h3 className=" text-2xl my-4 tracking-widest">
+              Webentwickler & Hobbyfotograf
+            </h3>
+          </div>
           <div>
             <Image
               src="/images/potrait-bw.png"
@@ -22,50 +31,6 @@ export default function Home() {
               alt="Potraitfoto"
             />
           </div>
-          <div>
-            <h2 className="font-black text-6xl mt-4 tracking-wide">Nader</h2>
-            <h2 className="font-black text-6xl tracking-wide">el Sayed</h2>
-
-            <h3 className="font-bold text-2xl my-4 tracking-widest">
-              Webentwickler
-            </h3>
-          </div>
-        </div>
-
-        <div className="tracking-wide sm:mt-10">
-          <p className="text-lg">
-            Herzlich Willkommen auf meinem Portfolio.
-            <br />
-            Ich bin ein 28-jähriger autodidaktischer Webentwickler aus der
-            Metropolregion Hamburg.
-          </p>
-          <p className="text-lg mt-4">
-            In den letzten Jahren habe ich mich besonders auf die Entwicklung
-            von Webapplikationen spezialisiert.
-          </p>
-          <p className="text-lg mt-4">
-            Technologien, die ich unter anderem einsetze:
-            <br /> HTML, CSS, TailwindCSS, JavaScript, React, Next.js, Node.js,
-            Express.js, MongoDB und Git.
-          </p>
-          <p className="text-lg my-4">
-            Mein Portfolio befindet sich derzeit im Aufbau. Schau doch solange
-            in mein neustes Projekt rein..
-          </p>
-        </div>
-
-        <div className="flex justify-center">
-          <a href="#project">
-            <AiOutlineArrowDown size="3rem" />
-          </a>
-        </div>
-
-        <div id="project" className="flex flex-col items-center">
-          <NewestProject
-            title={'EinkaufsGenie'}
-            image="/images/screenshot-einkaufsgenie.png"
-            link="https://einkaufsgenie.vercel.app"
-          />
         </div>
       </section>
     </>
